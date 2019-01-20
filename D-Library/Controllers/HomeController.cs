b4c72370e3cf9a551f15b77465b9594a99c67829
roozbeh shamekhi <roozbehshamekhi@hotmail.com>
2019-01-20 21:54:58 +0330

    2019_1_20_9_54

--- D-Library/Controllers/HomeController.cs
+++ D-Library/Controllers/HomeController.cs
@@ -35,6 +35,13 @@ namespace D_Library.Controllers
                 }
             }
         }
+
+        public ActionResult Uploader()
+        {
+
+            return View();
+        }
+
     }
 
 }
