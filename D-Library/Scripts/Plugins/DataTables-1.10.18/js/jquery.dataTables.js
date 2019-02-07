10025acaa240ba0c6b2d9be70d73f0edc000b53f
roozbeh shamekhi <roozbehshamekhi@hotmail.com>
2019-02-08 00:31:33 +0330

    2019_2_8_12_31

--- D-Library/Scripts/Plugins/DataTables-1.10.18/js/jquery.dataTables.js
+++ D-Library/Scripts/Plugins/DataTables-1.10.18/js/jquery.dataTables.js
@@ -4816,7 +4816,7 @@
 		var select = $('<select/>', {
 			'name':          tableId+'_length',
 			'aria-controls': tableId,
-			'class':         classes.sLengthSelect
+            'class':  'chosen-select'     
 		} );
 	
 		for ( var i=0, ien=lengths.length ; i<ien ; i++ ) {
@@ -14423,7 +14423,7 @@
 		"sFilterInput": "",
 	
 		/* Page length */
-		"sLengthSelect": "",
+        "sLengthSelect": "chosen-select",
 	
 		/* Scrolling */
 		"sScrollWrapper": "dataTables_scroll",
